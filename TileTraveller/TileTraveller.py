9b0833dd1ada6c3fb2b5bcf83c2ceaf0ad88afcb
Gummi007 <gudmundurr17@ru.is>
2018-09-18 09:30:24 +0000

    third commit

--- TileTraveller/TileTraveller.py
+++ TileTraveller/TileTraveller.py
@@ -13,4 +13,18 @@ while position != [3,1]:
             print("Not a valid direction!")
             direction = input("Direction: ")
             direction = direction.lower()
-        position[0] = 1+1  
+        position[1] = position[1] + 1  
+    elif position == [1,2]:
+        print ("You can travel: " + north + "or" + east + "or" + south + ".")
+        direction = input("Direction: ")
+        direction = direction.lower()
+        while direction != "n" or direction != "e" or direction != "s":
+            print("Not a valid direction!")
+            direction = input("Direction: ")
+            direction = direction.lower()
+        if direction == "n":
+            position[1] = position[1] + 1
+        elif direction == "e":
+            position[0] = position[0] + 1 
+        else:
+            position[1] = position[1] - 1
